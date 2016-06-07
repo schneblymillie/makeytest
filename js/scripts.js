e2ee11bd26c9b6c200be26418c90d4a0b5bdace4
schneblymillie <mschnebly@engr-131-01.eastern.ewu.edu>
2016-06-07 00:08:24 -0700

    images in

--- js/scripts.js
+++ js/scripts.js
@@ -22,27 +22,27 @@ $('html').keypress( function(e){
    console.log(e.keyCode);
     
     //Store Greetings
-var img1 = '../img/one.jpg'; 
-var img2 = '../img/two.jpg';
-var img3 = '../img/three.jpg';
-      var img4 = '../img/four.jpg';
-      var img5 = '../img/five.jpg';
-      var img6 = '../img/six.jpg';
-      var img7 = '../img/seven.jpg';
-      var img8 = '../img/eight.jpg';
-      var img9 = '../img/nine.jpg';
-      var img10 = '../img/ten.jpg';
-      var img11 = '../img/eleven.jpg';
-      var img12 = '../img/twelve.jpg';
-      var img13 = '../img/thirteen.jpg';
-      var img14 = '../img/fourteen.jpg';
-      var img15 = '../img/fifteen.jpg';
-      var img16 = '../img/sixteen.jpg';
-      var img17 = '../img/seventeen.jpg';
-      var img18 = '../img/eighteen.jpg';
-      var img19 = '../img/nineteen.jpg';
-      var img20 = '../img/twenty.jpg';
-      
+            var img1 = '../img/one.jpg'; 
+            var img2 = '../img/two.jpg';
+            var img3 = '../img/three.jpg';
+            var img4 = '../img/four.jpg';
+            var img5 = '../img/five.jpg';
+            var img6 = '../img/six.jpg';
+            var img7 = '../img/seven.jpg';
+            var img8 = '../img/eight.jpg';
+            var img9 = '../img/nine.jpg';
+            var img10 = '../img/ten.jpg';
+            var img11 = '../img/eleven.jpg';
+            var img12 = '../img/twelve.jpg';
+            var img13 = '../img/thirteen.jpg';
+            var img14 = '../img/fourteen.jpg';
+            var img15 = '../img/fifteen.jpg';
+            var img16 = '../img/sixteen.jpg';
+            var img17 = '../img/seventeen.jpg';
+            var img18 = '../img/eighteen.jpg';
+            var img19 = '../img/nineteen.jpg';
+            var img20 = '../img/twenty.jpg';
+
 
 //Get/make random number
 var limit = 20;
